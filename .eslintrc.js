aed47f75981b0e1350041d0ad731463b8000ffb5
Xifre Font <xifre.font.97@gmail.com>
2022-07-22 19:08:37 +0200

    Quick hotfix on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+
+  plugins: ["@typescript-eslint"],
+  rules: {},
 };
